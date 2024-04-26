66bca06ff7d989cf8c143f68150a7c4c164b5afd
realhardik18 <realhardik18@gmail.com>
2024-04-27 00:09:47 +0530

    working on calculating NRR

--- helpers.py
+++ helpers.py
@@ -14,5 +14,10 @@ def team_RR(score):
     score[0]=int(score[0].split('/')[0])
     return round(score[0]/score[-1],4)
 
+def score(data):
+    score=data.replace(' ','')
+    return int(score.split('/')[0])
+
+#print(score(" 224/4"))
 #print(team_RR(' 287/4, (50 ov) '))
 #print(match_NRR(' 287/4, (50 ov) ',' 243/4, (50 ov) '))
--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import pandas as pd
+from helpers import match_NRR,score
 
+teams_NRR={
+    #format:scored,played,conceeded,bowled
+    'SRH':[0,0,0,0],
+    'PBKS':[0,0,0,0],
+    'RR':[0,0,0,0],
+    'DC':[0,0,0],
+    'RCB':[0,0,0,0],
+    'MI':[0,0,0,0], 
+    'KKR':[0,0,0,0],
+    'GT':[0,0,0,0],
+    'CSK':[0,0,0,0],
+    'LSG':[0,0,0,0],
+}
 data=pd.read_csv('data.csv')
 
 for ind in data.index:
-    print(data['FIS'][ind])
+    teams=[data['BF'][ind],data['BS'][ind]]
+    WT=data['WT'][ind]
+    del teams[teams.index(WT)]
+    LT=teams[0]
+    teams_NRR['BF'][0]+=score(data['BF'][ind])
+
+
+
+print(teams_NRR)
